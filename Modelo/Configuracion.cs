69d01f395b4be82f3c4f6ecdb35e7a63fbe6981d
Juan Pablo <juanpablopela.34cj@gmail.com>
2020-11-11 09:46:23 -0500

    Agregar archivos de proyecto.

---
+++ Modelo/Configuracion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    class Configuracion
+    {
+        static string cadenaConexion = @"Data Source=DESKTOP-F5HOT1E;Initial Catalog=concesionarioWH; Trusted_Connection=true";
+        public static string CadenaConexion
+        {
+            get { return cadenaConexion; }
+        }
+    }
+}
